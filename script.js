8fb1e1bf825b6a92403b462af5da3a5ef177fb36
Edward <Ecampbell3015@gmail.com>
2022-07-05 19:15:01 -0700

    finish topbar

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let myLibrary = [];
 
+
+
+//Dom Elements ----//
+
+const createNewCard = document.querySelector('.createNew-window');
+const checkBox = document.getElementById('checkBox');
+const submitNew = document.querySelector('.submitNew');
+

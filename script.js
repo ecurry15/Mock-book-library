ffd9673d2b2443d35ed6077a1fb4a483c157f2df
Edward <Ecampbell3015@gmail.com>
2022-07-05 21:34:26 -0700

    add new book opens book creation card, creation card closes on submit or mouse click outside of card

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ let myLibrary = [];
 
 
 //Dom Elements ----//
-
 const createNewCard = document.querySelector('.createNew-window');
 const checkBox = document.getElementById('checkBox');
 const submitNew = document.querySelector('.submitNew');
+const totalBookNum = document.getElementById('totalBookNum');
+const bookReadNum = document.getElementById('bookReadNum');
+const addNew = document.querySelector('.addNew');
+const darkWrap = document.querySelector('.darkWrap');
 
+
+let bookMakerIsOpen = false;
+
+addNew.addEventListener('click', function() {
+  createNewCard.removeAttribute('id');
+  darkWrap.removeAttribute('id');
+})
+
+darkWrap.addEventListener('click', function() {
+ createNewCard.id = 'createNew-closed';
+ darkWrap.id = 'darkWrapOff';
+})
+
+submitNew.addEventListener('click', function() {
+  createNewCard.id = 'createNew-closed';
+  darkWrap.id = 'darkWrapOff';
+})
